8bfddc69c99fa5eba6a7583b1fb52b3a25117d14
Caio Emidio <caio.emidio@gmail.com>
2025-01-26 23:50:31 +0000

    feat: mock date until backend be ready

--- components/cards.vue
+++ components/cards.vue
@@ -5,23 +5,23 @@
 </template>
 
 <script setup>
-const { data } = await useAsyncData(() => $fetch("/api/info"));
+// const { data } = await useAsyncData(() => $fetch("/api/info"));
 
-// const data = [
-//   {
-//     photo: "https://cdn2.thecatapi.com/images/ctHlkAH3L.jpg",
-//     location: "National Stadium",
-//     name: "Sonderlin Festival",
-//     date: "12 Sept 2023",
-//     time: "8:00 PM",
-//   },
-//   {
-//     photo: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-//     location: "National Stadium",
-//     name: "Sonderlin Festival",
-//     date: "12 Sept 2023",
-//     time: "8:00 PM",
-//   },
-// ];
+const data = [
+  {
+    photo: "https://cdn2.thecatapi.com/images/ctHlkAH3L.jpg",
+    location: "National Stadium",
+    name: "Sonderlin Festival",
+    date: "12 Sept 2023",
+    time: "8:00 PM",
+  },
+  {
+    photo: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    location: "National Stadium",
+    name: "Sonderlin Festival",
+    date: "12 Sept 2023",
+    time: "8:00 PM",
+  },
+];
 
 </script>
